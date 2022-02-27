2b5977a1f2f75c59bc9316d15f64830b06b9fd46
Ng Jia Yong <46757018+callmeyonggor@users.noreply.github.com>
2022-02-27 11:14:53 +0800

    fix:package

--- database/seeders/InvoiceTableSeeder.php
+++ database/seeders/InvoiceTableSeeder.php
@@ -18,7 +18,6 @@ class InvoiceTableSeeder extends Seeder
         DB::table('invoice')->insert([
             'invoice_number' => 1,
             'invoice_total' => mt_rand(0.00,9999.99),
-            'status' => 0,
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
             'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
--- resources/views/invoice/listing.blade.php
+++ resources/views/invoice/listing.blade.php
@@ -3,6 +3,8 @@
 
 <head>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -12,9 +14,7 @@
                 <tr>
                     <th scope="col">Invoice_id</th>
                     <th scope="col">Invoice_number</th>
-                    <th scope="col">Item</th>
                     <th scope="col">Invoice_total</th>
-                    <th scope="col">Status</th>
                     <th scope="col">Created At</th>
                     <th scope="col">Updated At</th>
                 </tr>
@@ -24,9 +24,7 @@
                 <tr>
                     <td>{{$value->id}}</td>
                     <td>{{$value->invoice_number}}</td>
-                    <td>{{$value->item}}</td>
                     <td>{{$value->invoice_total}}</td>
-                    <td>{{$value->status}}</td>
                     <td>{{$value->created_at}}</td>
                     <td>{{$value->updated_at}}</td>
                     <td><a href="delete/{{$value->id}}">Delete</a></td>
