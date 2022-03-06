d6b81402eb0d1418ebff0f09dd0a1965e5220931
Ng Jia Yong <46757018+callmeyonggor@users.noreply.github.com>
2022-03-06 12:12:28 +0800

    Update filter.blade.php

--- resources/views/product/filter.blade.php
+++ resources/views/product/filter.blade.php
@@ -3,13 +3,6 @@
     <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
 </head>
 
-<<<<<<< Updated upstream:resources/views/product/listing.blade.php
-<body>
-    <form method="POST" action="{{ route('product_listing') }}">
-        @csrf
-        <div class="card">
-            <div class="cars-card-col">
-=======
 <body id="reload">
     <div class="card">
         <div class="cars-card-col">
@@ -19,36 +12,10 @@
                 </div>
             </div>
             <div class="container">
->>>>>>> Stashed changes:resources/views/product/filter.blade.php
                 <div class="row">
                     <div class="col-4">
                         {!! Form::select('category', $category_sel, @$category , ['class' => 'form-control select_active','id' => 'category']) !!}
                     </div>
-<<<<<<< Updated upstream:resources/views/product/listing.blade.php
-                </div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-3">
-                            <input type="text" class="form-control select_active" name="freetext" value="{{@$search['freetext']}}" placeholder="Search by keyword">
-                        </div>
-                        <div class="col-3">
-                            {!! Form::select('size', $size_sel, @$search['size'], ['class' => 'form-control select_active']) !!}
-                        </div>
-                        <div class="col-3">
-                            {!! Form::select('product_order_by', $product_order_by_sel, @$search['product_order_by'], ['class' => 'form-control select_active']) !!}
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <button type="submit" class="btn btn-success mr-2" name="submit" id="search" value="search">
-                        Search
-                    </button>
-
-                    <button type="submit" class="btn cars-btn-default" name="submit" value="reset">
-                        Reset
-                    </button>
-                </div>
-=======
                     <div class="col-4">
                         {!! Form::select('product_order_by', $product_order_by_sel, @$order_by, ['class' => 'form-control select_active','id' => 'order_by']) !!}
                     </div>
@@ -58,7 +25,6 @@
                 <button class="btn cars-btn-default">
                     <a href="reset_filter">Reset</a>
                 </button>
->>>>>>> Stashed changes:resources/views/product/filter.blade.php
             </div>
         </div>
     </div>
@@ -68,26 +34,17 @@
             <div class="col-3">
                 <div class="card-body">
                     <h5 class="card-header">{{$val->name}}</h5>
-<<<<<<< Updated upstream:resources/views/product/listing.blade.php
-                    <img src='/storage/img/{{$val->name}}.jpg' alt="blackshirt" class="img-thumbnail">
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-=======
                     <div class="border p-2">
                         <img src='/storage/img/{{$val->name}}.jpg' alt="{{$val->name}}" class="img-fluid img-thumbnail">
                         <p class="text-center">{{$val->unit_price}}</p>
                         <a href="/product/detail/{{$val->id}}" class="btn btn-info btn-block">{{$val->name}} Detail</a>
                     </div>
->>>>>>> Stashed changes:resources/views/product/filter.blade.php
                 </div>
             </div>
             @endforeach
         </div>
     </div>
 </body>
-<<<<<<< Updated upstream:resources/views/product/listing.blade.php
-
-</html>
-=======
 <script>
     $(document).ready(function() {
         $('#category').on('change', function() {
@@ -115,4 +72,3 @@
         })
     }
 </script>
->>>>>>> Stashed changes:resources/views/product/filter.blade.php
